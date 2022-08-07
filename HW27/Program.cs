5c7358b23d8297438dae2b6967fa5d9108dec854
martsel <108544179+martsel@users.noreply.github.com>
2022-08-07 22:34:41 +0300

    25,27,29

---
+++ HW27/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число  ");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while(number > 0)
+{
+    sum = sum + number % 10;
+    number /= 10;
+}
+Console.WriteLine("Результат = {sum}");
+// Console.WriteLine(sum);
+
+
+
+
+
+
+
+// Console.Write("Введите число А  ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+
+// while(A>=10)
+// {
+//     sum = A % 10;
+//     A = A/10;
+// }
+// Console.WriteLine($"Результат =  {sum}" );
+// Console.WriteLine(sum);
+
+
+// Console.Write("Введите число А  ");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// static void Main(string[] args)
+// {
+//     Console.Write("Введите число А  ");
+//     int A = Convert.ToInt32(Console.ReadLine());
+//     int sum = Foo(A);
+// }
+
+// static int Foo(int A)
+// {
+//     int sum = 1;
+//     while (A>=0)
+//     {
+//         sum += A % 10;
+//         A = A/10;
+//     }
+//     return sum;
+// }
+
+// // Console.WriteLine($"Результат =  {sum}" );
---
+++ HW29/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// (вводим массив с клавиатуры (т. е. через консоль :) ))
+
+
+// int[] arr = new int[7];
+// Console.Write("Введите первый элемент массива  ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// A = arr[0];
+
+
+
+
+// // Console.Write ($"{arr[7]} ");
+
+// Console.Write(string.Join(", ", arr));
+
+int[] arr = new int[7];
+for (int i = 0; i<=arr.Length; i++)
+{
+Console.Write($"Введите элемент масима под номером {i+1}  ");
+arr[i]=Convert.ToInt32(Console.ReadLine());
+}
+// Console.Write("Массив: ");
+// Console.Write ($"{arr[7]} ");
+Console.Write(string.Join(", ", arr));
+
+// for (int i = 0; i<=arr.Length; i++)
+// {
+//     Console.Write(arr[i]);
+// }
