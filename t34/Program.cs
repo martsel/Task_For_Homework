6feb1c3a44f5bf75aab4b0c8c332100063bb60af
martsel <108544179+martsel@users.noreply.github.com>
2022-08-10 23:12:50 +0300

    task34,36
    task34,36

---
+++ t34/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] array = new int [6];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+    Console.WriteLine(array[i]);
+}
+
+int summ = 0;
+for(int i = 1; i < array.Length; i++)
+{
+    if ((array[i] % 2) == 0)
+    {
+        summ++;
+    }
+}
+ Console.Write("sum");
+    Console.WriteLine(summ);
---
+++ t36/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int [] array = new int [6];
+Random rand = new Random();
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-10, 11);
+    Console.WriteLine(array[i]);
+}
+
+int summ = 0;
+for(int i = 1; i < array.Length; i+=2)
+{
+    summ += array[i];
+}
+ Console.Write("sum");
+    Console.WriteLine(summ);
