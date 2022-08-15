1d734a31913cac54800dd438f568c6358ca89d38
martsel <108544179+martsel@users.noreply.github.com>
2022-08-15 20:34:56 +0300

    41

---
+++ HWtask41/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел в одну линию 
+// (не используем Enter). Числа разделены пробелами. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0 7 8 -2 -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите числа:  ");
+int[] numbers = Console.ReadLine().Split().Select(x => Convert.ToInt32(x)).ToArray();
+int count = 0;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
