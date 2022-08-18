a82fe1d555516b891f3d95dd5f1d59877d75214e
martsel <108544179+martsel@users.noreply.github.com>
2022-08-18 20:25:12 +0300

    47 and 52

---
+++ HWTask47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задайте размерность двумерного массива m * n");
+        Console.Write("Введите m =  ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите n =  ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        double[,]a = new double[m,n];
+        Random random = new Random();
+        for (int i = 0; i < m; i++)
+        {
+             for (int j = 0; j < n; j++){
+                a[i, j] = random.NextDouble()*100;
+                Console.Write("{0,0:F2} ", a[i, j]);
+             }
+             Console.WriteLine();
+        }
+
+
+
+    }
+}
+
+
---
+++ t52/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Задайте размерность двумерного массива m * n");
+Console.Write("Введите m =  ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n =  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+Random ran = new Random();
+ double midle = 0;
+for (int i = 0; i < n; i++)
+{
+ 
+  for (int j = 0; j < m; j++)
+  {
+    matrix[i,j] = ran.Next (0,10);
+    midle += matrix[i, j];
+    Console.Write(matrix[i,j] + " , ");
+  }
+  midle = Math.Round(midle / m);
+  Console.WriteLine($"№ {i+1} = {midle}");
+}
+
