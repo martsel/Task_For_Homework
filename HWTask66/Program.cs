cbd9f6544b57690052ec4a73aae1c8db25a368cf
martsel <108544179+martsel@users.noreply.github.com>
2022-08-24 23:04:40 +0300

    Task 64, 66
    Task 64, 66

---
+++ HWTask66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите m =  ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите n =  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (m >= n)
+  {
+    Console.Write($"Сумма элементов = {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
